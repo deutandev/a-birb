a7a310aa5539c3dada3939c286e85cbbbe73a6c4
Andika Fathin Azfar <deutandev@pm.me>
2021-09-21 21:37:10 +0700

    Scripting Enemy

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameController : MonoBehaviour
 {
     public SlingShooter SlingShooter;
     public List<Bird> Birds;
+    // Tambahan Scripting Enemy
+    public List<Enemy> Enemies;
+
+    // Tambahan Scripting Enemy
+    private bool _isGameEnded = false;
+
 
     void Start()
     {
@@ -14,15 +21,46 @@ public class GameController : MonoBehaviour
         {
             Birds[i].OnBirdDestroyed += ChangeBird;
         }
+
+        // Tamabhan Scripting ENemy
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
         SlingShooter.InitiateBird(Birds[0]);
     }
 
     // Tambahan Eveny delegate
     public void ChangeBird()
     {
+        // Tambahan Scripting Enemy
+        if (_isGameEnded)
+        {
+            return;
+        }
+
         Birds.RemoveAt(0);
 
         if(Birds.Count > 0)
             SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    // Tambahan Scripting Enemy
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            if(Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if(Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
     }
 }
